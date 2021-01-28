8df41eb7b89cf03d7108e58375e081a46be88a36
Josh Long <josh@joshlong.com>
2021-01-27 23:31:26 -0800

    up

---
+++ code/customers/src/main/java/com/example/customers/CustomersApplication.java
@@ -0,0 +1,101 @@
+package com.example.customers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.reactive.socket.WebSocketHandler;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.util.Map;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+@RequiredArgsConstructor
+public class CustomersApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomersApplication.class, args);
+    }
+
+}
+
+@Configuration
+@RequiredArgsConstructor
+class WebSocketConfiguration {
+
+    private final ObjectMapper objectMapper;
+
+    private final AtomicInteger counter = new AtomicInteger();
+
+    private final String[] names =
+            ("Jean,Yuxin,Mario,Zhen,Mia,Maria,Dave,Johan,Francoise,Jose,Ibrahim")
+                    .split(",");
+
+    private final Flux<Customer> customers = Flux
+            .fromStream(Stream.generate(() -> {
+                var id = counter.incrementAndGet();
+                return new Customer(id, this.names[id % this.names.length]);
+            }))
+            .delayElements(Duration.ofSeconds(1));
+
+
+    @Bean
+    Flux<Customer> customers() {
+        return this.customers.publish().autoConnect();
+    }
+
+    @Bean
+    SimpleUrlHandlerMapping handlerMapping(
+            WebSocketHandler wsh) {
+        return new SimpleUrlHandlerMapping(Map.of("/ws/customers", wsh), 10);
+    }
+
+    @Bean
+    WebSocketHandler webSocketHandler() {
+        return session -> session
+                .send(this.customers()
+                        .map(this::toJson)
+                        .map(session::textMessage)
+                );
+    }
+
+    @SneakyThrows
+    private String toJson(Object customer) {
+        return objectMapper.writeValueAsString(customer);
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Customer {
+    private Integer id;
+    private String name;
+}
+
+@RestController
+class CustomerRestController {
+
+    private final Flux<Customer> customers;
+
+    CustomerRestController(Flux<Customer> customers) {
+        this.customers = customers;
+    }
+
+    @GetMapping(
+            value = "/customers",
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE
+    )
+    Flux<Customer> get() {
+        return this.customers;
+    }
+}
