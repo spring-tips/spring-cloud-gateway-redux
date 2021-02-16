231c2beb4ff1b1573e561f2656feb22a6e99a764
Josh Long <josh@joshlong.com>
2021-02-15 21:19:55 -0800

    Update CustomersApplication.java

--- code/customers/src/main/java/com/example/customers/CustomersApplication.java
+++ code/customers/src/main/java/com/example/customers/CustomersApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.reactive.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.reactive.socket.WebSocketHandler;
@@ -19,93 +21,87 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Stream;
 
+import static org.springframework.http.HttpStatus.SERVICE_UNAVAILABLE;
+
 @SpringBootApplication
-@RequiredArgsConstructor
 public class CustomersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CustomersApplication.class, args);
     }
 
-}
+    private final String[] names =
+            "Jean,Yuxin,Mario,Zhen,Mia,Maria,Dave,Johan,Francoise,Jose,Ibrahim".split(",");
 
-@Configuration
-@RequiredArgsConstructor
-class WebSocketConfiguration {
-
-    private final ObjectMapper objectMapper;
 
     private final AtomicInteger counter = new AtomicInteger();
 
-    private final String[] names =
-            ("Jean,Yuxin,Mario,Zhen,Mia,Maria,Dave,Johan,Francoise,Jose,Ibrahim")
-                    .split(",");
-
-    private final Flux<Customer> customers = Flux
-            .fromStream(Stream.generate(() -> {
+    private final Flux<Customer> customers = Flux.fromStream(
+            Stream.generate(() -> {
                 var id = counter.incrementAndGet();
-                return new Customer(id, this.names[id % this.names.length]);
+                return new Customer(id, names[id % names.length]);
             }))
             .delayElements(Duration.ofSeconds(3));
-
 
     @Bean
     Flux<Customer> customers() {
         return this.customers.publish().autoConnect();
     }
 
-    @Bean
-    SimpleUrlHandlerMapping handlerMapping(
-            WebSocketHandler wsh) {
-        return new SimpleUrlHandlerMapping(Map.of("/ws/customers", wsh), 10);
-    }
 
-    @Bean
-    WebSocketHandler webSocketHandler() {
-        return session -> session
-                .send(this.customers()
-                        .map(this::toJson)
-                        .map(session::textMessage)
-                );
-    }
+}
+
+
+@Configuration
+@RequiredArgsConstructor
+class CustomerWebSocketConfiguration {
+
+
+    private final ObjectMapper objectMapper;
 
     @SneakyThrows
-    private String toJson(Object customer) {
-        return objectMapper.writeValueAsString(customer);
+    private String from(Customer customer) {
+        return this.objectMapper.writeValueAsString(customer);
+    }
+
+    @Bean
+    WebSocketHandler webSocketHandler(Flux<Customer> customerFlux) {
+        return webSocketSession -> {
+
+            var map = customerFlux
+                    .map(customer -> from(customer))
+                    .map(webSocketSession::textMessage);
+
+            return webSocketSession.send(map);
+        };
+    }
+
+    @Bean
+    SimpleUrlHandlerMapping simpleUrlHandlerMapping(WebSocketHandler customersWsh) {
+        return new SimpleUrlHandlerMapping(Map.of("/ws/customers", customersWsh), 10);
     }
 }
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
-class Customer {
-    private Integer id;
-    private String name;
-}
-
 @RestController
+@RequiredArgsConstructor
 class CustomerRestController {
 
-    private final Flux<Customer> customers;
-
-    CustomerRestController(Flux<Customer> customers) {
-        this.customers = customers;
-    }
-
+    private final Flux<Customer> customerFlux;
 
     @GetMapping(
-            value = "/customers",
-            produces = MediaType.TEXT_EVENT_STREAM_VALUE
+            produces = MediaType.TEXT_EVENT_STREAM_VALUE,
+            value = "/customers"
     )
     Flux<Customer> get() {
-        return this.customers;
+        return this.customerFlux;
     }
+
 }
 
-// dont create this until the rate limiting section
 @RestController
-class RateLimitingRestController {
+class ReliabilityRestController {
 
+    private final Map<String, AtomicInteger> errorCount = new ConcurrentHashMap<>();
     private final Map<Long, AtomicInteger> secondToCount = new ConcurrentHashMap<>();
 
     @GetMapping("/hello")
@@ -118,12 +114,41 @@ class RateLimitingRestController {
             value.incrementAndGet();
             return value;
         });
-
         System.out.println("there have been " + countForTheSecond + " attempts for the second " + second + '.');
-
-
-        // todo capture the current second and how many requests we've seen in this second
         return "Hello, world";
     }
 
+
+    @GetMapping("/error/{id}")
+    ResponseEntity<?> errors(@PathVariable String id) {
+
+        this.errorCount.compute(id,
+                (s, value) -> {
+                    if (value == null) {
+                        value = new AtomicInteger(0);
+                    }
+                    value.incrementAndGet();
+                    return value;
+                });
+
+
+        var count = this.errorCount.get(id).get();
+        if (count < 5) {
+            System.out.println("returning an error for request #" + count + " for ID '" + id + "'");
+            return ResponseEntity.status(SERVICE_UNAVAILABLE).build();
+        } else {
+            System.out.println("returning a proper response for request #" + count + " for ID '" + id + "'");
+        }
+        return ResponseEntity.ok(
+                Map.of("greeting", "Congrats, " + id + "! You did it!"));
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class Customer {
+
+    private Integer id;
+    private String name;
 }
