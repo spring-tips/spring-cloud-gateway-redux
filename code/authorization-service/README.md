3071d7c98fd37061abd16000a1ab65f7d8d95762
Josh Long <josh@joshlong.com>
2021-02-15 16:12:54 -0800

    auth begone

--- code/authorization-service/README.md
+++
@@ -1,1 +0,0 @@
-This code came from this excellent <a href="https://www.baeldung.com/keycloak-embedded-in-spring-boot-app">article on the Baeldung blog</a>
--- code/authorization-service/src/main/java/com/baeldung/authserver/config/RegularJsonConfigProviderFactory.java
+++
@@ -1,7 +0,0 @@
-package com.baeldung.authserver.config;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
-
-}
