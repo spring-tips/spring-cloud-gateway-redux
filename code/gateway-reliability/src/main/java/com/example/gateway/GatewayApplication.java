d6c5d6ceaa94c50531224a34a85d367ff49ac2f9
Josh Long <josh@joshlong.com>
2021-02-15 21:21:14 -0800

    Update GatewayApplication.java

--- code/gateway-reliability/src/main/java/com/example/gateway/GatewayApplication.java
+++ code/gateway-reliability/src/main/java/com/example/gateway/GatewayApplication.java
@@ -1,99 +1,96 @@
-package com.example.gateway;
+package com.example.basics;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.gateway.event.RefreshRoutesResultEvent;
-import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
-import org.springframework.cloud.gateway.filter.factory.RewritePathGatewayFilterFactory;
-import org.springframework.cloud.gateway.route.CachingRouteLocator;
-import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.PrincipalNameKeyResolver;
+import org.springframework.cloud.gateway.filter.ratelimit.RedisRateLimiter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
-import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
-import org.springframework.util.Assert;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
-
-/**
- * Here are some of the URLs that work:
- *
- * <LI> http://localhost:9292/customers/customers </LI>
- * <LI> http://localhost:9292/orders/orders/2 </LI>
- * <LI> http://localhost:9292/hello </LI>
- * <LI> http://localhost:9292/new-customers.ws </LI>
- * <LI> http://localhost:9292/index.html </LI>
- * <LI> http://localhost:9292/actuator/gateway </LI>
- * <LI> http://localhost:9292/actuator/metrics/spring.cloud.gateway.requests </LI>
- * <LI> http://localhost:9292/o/3.json </LI>
- * <LI> http://localhost:9292/twitter/@starbuxman </LI>
- */
-@Log4j2
 @SpringBootApplication
 public class GatewayApplication {
-
-
-    @Bean
-    ApplicationListener<RefreshRoutesResultEvent> routesRefreshed() {
-        return rre -> {
-            log.info(rre.getClass().getSimpleName());
-            Assert.state(rre.getSource() instanceof CachingRouteLocator, () -> "the " + rre.getClass().getName() + " routes must be refreshed");
-            CachingRouteLocator source = (CachingRouteLocator) rre.getSource();
-            Flux<Route> routes = source.getRoutes();
-            routes.subscribe(route -> log.info(route.getClass() + ":"
-                    + route.getMetadata().toString() + ":" + route.getFilters()));
-        };
-    }
-
-
-    @Bean
-    RouteLocator gateway(RouteLocatorBuilder rlb) {
-        return rlb
-                .routes()
-                .route(routeSpec -> routeSpec.path("/hello").filters(fs -> fs.setPath("/guides")).uri("http://spring.io")) // http
-                .route(routeSpec -> routeSpec.path("/new-customers.ws").filters(fs -> fs.setPath("/ws/customers")).uri("lb://customers/")) // websockets
-                .route(rs -> rs
-                        .path("/twitter/@**")
-                        .filters(c -> c.rewritePath("/twitter/@(?<handle>.*)", "/${handle}"))
-                        .uri("http://twitter.com/")
-                )
-                .build();
-    }
-
-    @Bean
-    RouteLocator customRouteLocator(RewritePathGatewayFilterFactory rewritePathGatewayFilterFactory) {
-
-        var rewritePathGatewayFilter = rewritePathGatewayFilterFactory
-                .apply(config -> config
-                        .setRegexp("\\/o\\/(?<segment>.*)\\.json")
-                        .setReplacement("/orders/${segment}")
-                );
-
-        var singleRoute = Route//
-                .async() //
-                .id("orders-json-to-orders") //
-                .asyncPredicate(request -> {
-                    var uri = request.getRequest().getURI();
-                    var path = uri.getPath();
-                    var match = path.contains("o/");
-                    log.debug("result for " + uri + '/' + path + " " + match);
-                    return Mono.just(match);
-                })
-                .filter(new OrderedGatewayFilter(rewritePathGatewayFilter, 0)) //
-                .filter(new OrderedGatewayFilter((exchange, chain) -> {
-                    log.info("new URI: " + exchange.getRequest().getURI());
-                    return chain.filter(exchange);
-                }, 1))
-                .uri("lb://orders")
-                .build();
-
-        return () -> Flux.just(singleRoute);//
-    }
 
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
 
+    @Bean
+    RouteLocator gateway(RouteLocatorBuilder routeLocatorBuilder) {
+
+        return routeLocatorBuilder
+                .routes()
+                .route( rs -> rs   .path("/expensive")
+                        .filters(fs -> fs
+                                .requestRateLimiter(rlc -> rlc
+                                        .setRateLimiter(rrl())
+                                        .setKeyResolver(pks())
+                                )
+                                .setPath("/hello")
+                        )
+                        .uri("lb://customers"))
+                .route(rs -> rs
+                        .path("/default")
+                        .filters(fs -> fs
+                                .setPath("/")
+                                .filter((exchange, chain) -> {
+                                    System.out.println("new chance!");
+                                    return chain.filter(exchange);
+                                })
+                        )
+                        .uri("https://spring.io/guides")
+                )
+                .route(rs -> rs
+                        .path("/customers")
+                        .filters(gfs -> gfs.circuitBreaker(config -> config
+                                .setFallbackUri("forward:/default")
+                        ))
+                        .uri("lb://customers")
+                )
+                .route(rs -> rs
+                        .path("/error/**")
+                        .filters(fs -> fs.retry(5))
+                        .uri("lb://customers")
+                )
+                .build();
+    }
+
+    @Bean
+    RedisRateLimiter rrl() {
+        return new RedisRateLimiter(5, 10);
+    }
+
+    @Bean
+    KeyResolver pks() {
+        return new PrincipalNameKeyResolver();
+    }
+
+}
+
+
+@Configuration
+class SecurityConfiguration {
+
+    @Bean
+    SecurityWebFilterChain authorization(ServerHttpSecurity http) {
+        return http
+                .authorizeExchange(ae ->
+                        ae.pathMatchers("/hi").authenticated()
+                )
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .httpBasic(Customizer.withDefaults())
+                .build();
+    }
+
+    @Bean
+    MapReactiveUserDetailsService authentication() {
+        return new MapReactiveUserDetailsService(User.withDefaultPasswordEncoder().username("jlong").password("pw").roles("USER").build());
+    }
 }
